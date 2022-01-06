4ae530117286114f59d56c122941212d9b366a5d
Randy <randykeller1193@gmail.com>
2022-01-05 20:57:49 -0500

    working prototype

--- src/App.js
+++ src/App.js
@@ -10,21 +10,42 @@ import { WebrtcProvider } from "y-webrtc";
 import { proxy, subscribe, useSnapshot } from "valtio";
 
 const ydoc = new Y.Doc();
-const provider = new WebrtcProvider("test", ydoc);
 const ymap = ydoc.getMap("map");
+const provider = new WebrtcProvider("test2", ydoc);
 
-const avatarStore = proxy({});
+const chatStore = proxy({});
 bindProxyAndYMap(chatStore, ymap);
 
 function App() {
-  provider.on("peers", (e) => setNewPeer(console.log(e)));
+  // provider.on("peers", (e) => console.log(e));
 
   const snap = useSnapshot(chatStore);
   // const otherUsers = Object.keys({ ...snap }).filter(
   //   (user) => user != username
   // );
 
-  return <div>connected</div>;
+  const [button, setButton] = useState("");
+
+  useEffect(() => {
+    console.log(snap);
+  }, [snap]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={button}
+        onChange={(e) => setButton(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          chatStore[button] = "🏆";
+        }}
+      >
+        submit
+      </button>
+    </div>
+  );
 }
 
 export default App;
